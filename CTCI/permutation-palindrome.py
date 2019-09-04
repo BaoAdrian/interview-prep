765e92c9e88af431eb58ea142c9c7c5d5abec7f5
Adrian Bao <adrianbao14@gmail.com>
2019-09-03 18:14:55 -0700

    Initial commit

---
+++ CTCI/permutation-palindrome.py
@@ -0,0 +1,26 @@
+
+
+def per_pal(string):
+    counts = {}
+    odd_count = False
+    string = string.lower()
+    for char in string:
+        if char != " ":
+            if char not in counts:
+                counts[char] = 1
+            else:
+                counts[char] += 1
+
+    for count in counts.values():
+        if count % 2 != 0:
+            if odd_count == True:
+                return False
+            else:
+                odd_count = True
+    return True
+
+def main():
+    print(per_pal("Tact coa"))
+
+if __name__ == "__main__":
+    main()
