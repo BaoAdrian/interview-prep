e7a21c1db00dbb43142b8e6d05ffde386cde8ff9
Adrian Bao <adrianbao14@gmail.com>
2019-10-13 18:57:27 -0700

    Update unival tree solution

--- coding-problems/leetcode/trees/unival_tree.py
+++ coding-problems/leetcode/trees/unival_tree.py
@@ -11,6 +11,22 @@
 #         self.right = None
 
 class Solution:
+
+    def isUnivalTree(self, root):
+        """
+        Cleaner Solution
+        Time: 12ms (94.56%)
+        Space: 11.9MB (16.67%)
+        """
+        if not root:
+            return True
+        else:
+            if root.left and root.left.val != root.val:
+                return False
+            if root.right and root.right.val != root.val:
+                return False
+            return self.isUnivalTree(root.left) and self.isUnivalTree(root.right)
+
     def isUnivalTree(self, root: TreeNode) -> bool:
         if root is None or (root.right is None and root.left is None):
             return True
